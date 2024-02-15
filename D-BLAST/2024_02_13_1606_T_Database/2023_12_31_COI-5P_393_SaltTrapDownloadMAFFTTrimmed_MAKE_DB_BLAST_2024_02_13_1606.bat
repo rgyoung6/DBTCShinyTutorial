f2098b221403c0ebba1c475285fb0feaceb0818a
rgyoung6 <rgyoung6@gmail.com>
2024-02-15 11:54:32 -0500

    Updated the makeBLASTdb function and sample files

--- D-BLAST/2024_02_13_1606_T_Database/2023_12_31_COI-5P_393_SaltTrapDownloadMAFFTTrimmed_MAKE_DB_BLAST_2024_02_13_1606.bat
+++
@@ -1,1 +0,0 @@
-makeblastdb -in 2023_12_31_COI-5P_393_SaltTrapDownloadMAFFTTrimmed_DB_Create_File_2024_02_13_1606.fas  -parse_seqids -dbtype nucl -out T
---
+++ D-BLAST/2024_02_15_1149_ST_Database/2023_12_31_COI-5P_393_SaltTrap_MAKE_DB_BLAST_2024_02_15_1149.bat
@@ -0,0 +1,1 @@
+E:\A_Test_DBTCShiny\makeblastdb.exe -in 2023_12_31_COI-5P_393_SaltTrap_DB_Create_File_2024_02_15_1149.fas  -parse_seqids -dbtype nucl -out ST
